59b811c4671edbc6030b1d3126aec13fb09be76a
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-06 12:49:05 +0800

    expression1

---
+++ OperatorsAssociavity/OperatorsAssociavity/src/expression.java
@@ -0,0 +1,11 @@
+public class expression {
+    public static void main(String[] args) {
+        byte x =7;
+        short y =9;
+        int z=3;
+        float p =6.9f+x;
+        int q = y+z;
+        System.out.println(p);
+        System.out.println(q);
+    }
+}
