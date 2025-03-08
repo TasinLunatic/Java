030dadcbc864ff7de8867d6153425d06da392338
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-08 20:43:13 +0800

    prob1 done

---
+++ OperatorsAssociavity/OperatorsAssociavity/src/ProblemSolving.java
@@ -0,0 +1,9 @@
+public class ProblemSolving {
+    public static void main(String[] args) {
+
+        //What will be the result of the following expression
+        //float a =7/4*9/2;
+        float a = 7.0f/4.0f*9.0f/2.0f;
+        System.out.println(a);
+    }
+}
