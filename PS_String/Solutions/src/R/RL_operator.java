d5f04f6fa7be301ecde2b381b40074ffc9d5f8c4
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-17 00:58:21 +0800

    Yes No done

---
+++ PS_String/Solutions/src/R/RL_operator.java
@@ -0,0 +1,13 @@
+package R;
+
+public class RL_operator {
+    public static void main(String[] args) {
+        boolean a =true;
+        boolean b =false ;
+        if(a && b){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
--- PS_String/Solutions/src/ifelse_condition.java
+++ PS_String/Solutions/src/ifelse_condition.java
@@ -1,9 +1,9 @@
 public class ifelse_condition {
     public static void main(String[] args) {
         int age = 19 ;
-        if(age>19){
+        if(age > 19){
              System.out.println("You can drive");
-        } else if (age ==19) {
+        } else if (age == 19) {
             System.out.println("you can drive now");
 
         } else {
