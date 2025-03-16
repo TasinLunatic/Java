de1e2d5621ae46c39603e2760862e658fdc9472c
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-17 01:06:46 +0800

    And Or done

--- PS_String/Solutions/src/R/RL_operator.java
+++ PS_String/Solutions/src/R/RL_operator.java
@@ -2,9 +2,21 @@ package R;
 
 public class RL_operator {
     public static void main(String[] args) {
+        System.out.println("For Logical And...");
         boolean a =true;
-        boolean b =false ;
-        if(a && b){
+        boolean b =true ;
+        boolean c =true ;
+        if(a && b && c){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+
+        System.out.println("For Logical Or...");
+        boolean a1 =true;
+        boolean b1 =false ;
+        boolean c1 = false ;
+        if(a1 || b1 ||c1){
             System.out.println("YES");
         }else {
             System.out.println("NO");
