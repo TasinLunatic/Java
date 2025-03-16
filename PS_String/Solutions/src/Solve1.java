5f2ff8c880a96b7e4eba2a5633b11673e5533998
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-16 23:04:11 +0800

    Problem 1 to 3 solved

--- PS_String/Solutions/src/Solve1.java
+++ PS_String/Solutions/src/Solve1.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+
 public class Solve1 {
     public static void main(String[] args) {
+        //Write a java program to convert string in lower case?
+        String name ="Pablo Escober" ;
+        String lcase = name.toLowerCase(Locale.ROOT) ;
+        System.out.println(name);
+
+        //Write a java program to replace space to underscore ?
+        String text = "Hello Boys." ;
+        text = text.replace(" ","_") ;
+        System.out.println(text);
+
+        //Write a java program to fill in a letter template like this
+        // letter = "Dear <|name|> , thanks a lot."
+        //Replace name with a string name..?
+        String letter = "Dear <|name|>,Thanks a lot.";
+        letter = letter.replace("<|name|>","Tasin") ;
+        System.out.println(letter);
+
 
     }
 }
