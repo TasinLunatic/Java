f981b9cce12d566bfdb51ad357d8842ddab78dd8
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-16 23:23:28 +0800

    All problem solved

---
+++ PS_String/Solutions/src/Solve2.java
@@ -0,0 +1,10 @@
+public class Solve2 {
+    public static void main(String[] args) {
+        //Write a java program to format the following letter using escape sequence characters
+        //letter ="Dear Teacher , This java course is so nice and easy . thanks."
+        String letter =  "Dear Teacher,\n\tThis java course is so nice and easy.\n\tThanks you teacher." ;
+        System.out.println(letter);
+
+
+    }
+}
