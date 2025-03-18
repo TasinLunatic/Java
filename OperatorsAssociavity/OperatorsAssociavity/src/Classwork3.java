de5e259adf6766f4b14901447f4dd083f0345d2f
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-19 01:50:03 +0800

    new scanner added

--- OperatorsAssociavity/OperatorsAssociavity/src/Classwork3.java
+++ OperatorsAssociavity/OperatorsAssociavity/src/Classwork3.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class Classwork3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in) ;
+        Scanner sc1 = new Scanner(System.in) ;
+        Scanner sc2 = new Scanner(System.in) ;
 
         System.out.println("Enter three points : ");
 
         double x1 = sc.nextDouble();
         double y1 = sc.nextDouble();
-        double x2 = sc.nextDouble();
-        double y2 = sc.nextDouble();
-        double x3 = sc.nextDouble();
-        double y3 = sc.nextDouble();
+        double x2 = sc1.nextDouble();
+        double y2 = sc1.nextDouble();
+        double x3 = sc2.nextDouble();
+        double y3 = sc2.nextDouble();
 
         double a = Math.sqrt((x2-x3)*(x2-x3)+ (y2-y3)*(y2-y3));
         double b = Math.sqrt((x1-x3)*(x1-x3)+ (y1-y3)*(y1-y3));
