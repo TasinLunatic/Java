3d687f068d1456611b382daad9f7a5d3dae5c2dc
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-19 15:44:11 +0800

    Rounding done

---
+++ Lecture/src/Rounding.java
@@ -0,0 +1,12 @@
+public class Rounding {
+    public static void main(String[] args) {
+        System.out.println("ceil(2.1) is " +Math.ceil(2.1));
+        System.out.println("ceil(2.0) is " +Math.ceil(2.0));
+        System.out.println("ceil(-2.0) is " +Math.ceil(-2.0));
+        System.out.println("ceil(-2.1) is " +Math.ceil(-2.1));
+
+        System.out.println("floor(2.1) is " + Math.floor(2.1)) ;
+        System.out.println("floor(2.0) is " + Math.floor(2.0)) ;
+        System.out.println("floor(-2.1) is " + Math.floor(-2.1));
+    }
+}
