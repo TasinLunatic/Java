989ec0eaf169744a5b9c0809434cc75706ffc7d9
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 01:42:03 +0800

    Find web done

--- PS_String/Solutions/src/R/FindWeb.java
+++ PS_String/Solutions/src/R/FindWeb.java
@@ -9,15 +9,15 @@ public class FindWeb {
         String website =sc.next();
 
         if (website.endsWith(".org")){
-            System.out.println("This is an organizational website");
+            System.out.println("This is an organizational website.");
         } else if (website.endsWith(".com")) {
-            System.out.println("This is a commercial website");
+            System.out.println("This is a commercial website.");
         } else if (website.endsWith(".in")) {
-            System.out.println("This is an indian website");
+            System.out.println("This is an indian website.");
         } else if (website.endsWith(".bd")) {
-            System.out.println("This is a Bangladeshi website");
+            System.out.println("This is a Bangladeshi website.");
         }else if (website.endsWith(".cn")){
-            System.out.println("This is an chinese website");
+            System.out.println("This is an chinese website.");
         }
     }
 }
