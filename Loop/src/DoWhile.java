10e070ebbfefef1a602c8f92506c93cccd18671a
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 02:35:38 +0800

    intro done

---
+++ Loop/src/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int a=0;
+
+        do {
+            System.out.println(a);
+            a++;
+        }while (a<5);
+    }
+}
