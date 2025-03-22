11584c5bc5e2a7d09b13a16276ea0791db6bf1af
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 01:59:32 +0800

    main code done

---
+++ PS_String/Solutions/src/R/RPSgame.java
@@ -0,0 +1,34 @@
+package R;
+import java.util.Random ;
+import java.util.Scanner;
+
+public class RPSgame {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"Rock", "Paper", "Scissors"};
+
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.print("Enter your choice (Rock, Paper, Scissors): ");
+        String userChoice = scanner.nextLine();
+
+        int computerIndex = random.nextInt(3);
+        String computerChoice = choices[computerIndex];
+
+        System.out.println("Computer chose: " + computerChoice);
+
+        if (userChoice.equalsIgnoreCase(computerChoice)) {
+            System.out.println("It's a tie!");
+        } else if ((userChoice.equalsIgnoreCase("Rock") && computerChoice.equals("Scissors")) ||
+                (userChoice.equalsIgnoreCase("Paper") && computerChoice.equals("Rock")) ||
+                (userChoice.equalsIgnoreCase("Scissors") && computerChoice.equals("Paper"))) {
+                System.out.println("You win!");
+        } else {
+            System.out.println("You lose!");
+                }
+
+
+    }
+}
