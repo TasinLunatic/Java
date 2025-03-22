6076057db4df8e7d9069a75612b92d02ff4d3001
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-22 16:48:25 +0800

    Day Name done

---
+++ PS_String/Solutions/src/R/Calendar.java
@@ -0,0 +1,19 @@
+package R;
+
+public class Calendar {
+    public static void main(String[] args) {
+        //Write a java program to find out the day of the week given the number[1 for monday 2 for tues and so on...]
+        int day =2;
+
+        switch (day){
+            case 1 ->System.out.println("Monday");
+            case 2 ->System.out.println("Tuesday");
+            case 3 ->System.out.println("Wednesday");
+            case 4 ->System.out.println("Thursday");
+            case 5 ->System.out.println("Friday");
+            case 6 ->System.out.println("Saturday");
+            case 7 ->System.out.println("Sunday");
+
+        }
+    }
+}
