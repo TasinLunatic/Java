f321e1066c4162000a4fbcef7b3984c96fc84b0b
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 02:08:42 +0800

    Testing class on git

---
+++ Loop/src/DoWhile.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class DoWhile {
+    public static void main(String[] args) {
+        System.out.println("Do While loop started!");
+    }
+}
