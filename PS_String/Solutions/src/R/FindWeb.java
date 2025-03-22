1bdd9bd3296b47a20224ef12de7f77e0cb072d12
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 01:39:22 +0800

    website region added

--- PS_String/Solutions/src/R/FindWeb.java
+++ PS_String/Solutions/src/R/FindWeb.java
@@ -12,6 +12,12 @@ public class FindWeb {
             System.out.println("This is an organizational website");
         } else if (website.endsWith(".com")) {
             System.out.println("This is a commercial website");
+        } else if (website.endsWith(".in")) {
+            System.out.println("This is an indian website");
+        } else if (website.endsWith(".bd")) {
+            System.out.println("This is a Bangladeshi website");
+        }else if (website.endsWith(".cn")){
+            System.out.println("This is an chinese website");
         }
     }
 }
