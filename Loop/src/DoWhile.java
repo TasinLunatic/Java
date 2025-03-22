f4a39d8dd30d5ee352e3b8611796daecb88a4d08
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 02:45:43 +0800

    First part done

--- Loop/src/DoWhile.java
+++ Loop/src/DoWhile.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class DoWhile {
     public static void main(String[] args) {
+        //do while first execute code then check true or false
+
         int a=0;
 
         do {
             System.out.println(a);
             a++;
         }while (a<5);
+
+        //write a program using do while loop to print first n natural number?
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the value of n: ");
+        int n= sc.nextInt();
+
     }
 }
