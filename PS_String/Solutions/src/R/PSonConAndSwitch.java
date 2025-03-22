028d3ea1668bfca511c2692a68f24035fe40a35d
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-22 15:55:44 +0800

    Prob2 solved

--- PS_String/Solutions/src/R/PSonConAndSwitch.java
+++ PS_String/Solutions/src/R/PSonConAndSwitch.java
@@ -21,7 +21,16 @@ public class PSonConAndSwitch {
          System.out.println("Enter your Chemistry mark: ");
          m2 =sc.nextByte() ;
 
-         System.out.println("Enter your Mathmatics mark: ");
+         System.out.println("Enter your Math mark: ");
+         m3 = sc.nextByte() ;
+
+         float avg = (m1+m2+m3)/3.0f ;
+
+         if (avg>=40 && m1>=33 && m2>=33 && m3>=33){
+             System.out.println("Congratulation, You passed!");
+         }else {
+             System.out.println("Sorry! You have not promoted.");
+         }
 
     }
 }
