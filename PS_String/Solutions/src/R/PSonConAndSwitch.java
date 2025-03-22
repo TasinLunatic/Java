27db57a9d2b8873d7a9866be2c4bc1b875d83bbc
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-22 14:00:53 +0800

    problem 1 solved

---
+++ PS_String/Solutions/src/R/PSonConAndSwitch.java
@@ -0,0 +1,13 @@
+package R;
+
+public class PSonConAndSwitch {
+    public static void main(String[] args) {
+        //Prob1. What will the output of the program?
+        int a=10;
+        if(a==11){
+            System.out.println("I am 11 !");
+        }else {
+            System.out.println("I am not 11 .");
+        }
+    }
+}
