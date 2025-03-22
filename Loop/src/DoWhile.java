ca6276e3457e29c0689390b5a55bc34e5015a640
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-23 02:17:19 +0800

    intro

--- Loop/src/DoWhile.java
+++
@@ -1,7 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class DoWhile {
-    public static void main(String[] args) {
-        System.out.println("Do While loop started!");
-    }
-}
---
+++ Loop/src/WhileLoop.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class WhileLoop {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println("Using Loops : ");
+
+        int i=1;
+        while (i<=3){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
