22e9aee4b9325a2f9def068a851983a52a297685
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-22 16:39:51 +0800

    Mathmatical problem solved and Program done

--- PS_String/Solutions/src/R/IncomeTaxCalculate.java
+++ PS_String/Solutions/src/R/IncomeTaxCalculate.java
@@ -15,14 +15,14 @@ public class IncomeTaxCalculate {
         float tax =0;
         float income = sc.nextFloat();
 
-        if(income<2.5f){
+        if(income<=2.5f){
             tax =tax + 0 ;
         }
-        else if (income > 2.5f && income < 5f) {
+        else if (income > 2.5f && income <= 5f) {
             tax = tax +0.05f*(income - 2.5f);
             
         }
-        else if (income>5f && income<10f) {
+        else if (income>5f && income<=10f) {
             tax =tax +0.05f*(5.0f-2.5f);
             tax = tax +0.2f*(income - 2.5f);
         }
