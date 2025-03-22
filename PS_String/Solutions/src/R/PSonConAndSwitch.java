2fa340ef169eb534ed1dcea9a87a4e1295bad85a
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-22 14:13:56 +0800

    First part done

--- PS_String/Solutions/src/R/PSonConAndSwitch.java
+++ PS_String/Solutions/src/R/PSonConAndSwitch.java
@@ -1,13 +1,27 @@
 package R;
+import java.util.Scanner;
 
 public class PSonConAndSwitch {
     public static void main(String[] args) {
         //Prob1. What will the output of the program?
-        int a=10;
+        /* int a=10;
         if(a==11){
             System.out.println("I am 11 !");
         }else {
             System.out.println("I am not 11 .");
-        }
+        }*/
+
+        //Prob2. Write a program to find out whether a student pass or fail
+        //if it requires total 40% and at least 33% each subject to pass.Assume 3 subjects and take marks as an input from the user.
+         byte m1,m2,m3 ;
+         Scanner sc =new Scanner(System.in) ;
+         System.out.println("Enter your Physics mark: ");
+         m1 =sc.nextByte();
+
+         System.out.println("Enter your Chemistry mark: ");
+         m2 =sc.nextByte() ;
+
+         System.out.println("Enter your Mathmatics mark: ");
+
     }
 }
