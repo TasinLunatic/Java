936c3a6150da0f3a84e77ff20168d848cb510774
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-25 01:43:46 +0800

    Primary test done

---
+++ Loop/src/BreakandContinue.java
@@ -0,0 +1,9 @@
+public class BreakandContinue {
+    public static void main(String[] args) {
+        //break and continue using loops!
+        for(int i=1;i<5;i++){
+            System.out.println(i);
+            System.out.println("Java is amazing");
+        }
+    }
+}
