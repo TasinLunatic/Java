e45cd17b2e2ef23cac6fe2d26b7774debef9a104
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-27 01:34:50 +0800

    Maximum test done

---
+++ Lecture/src/TestMax.java
@@ -0,0 +1,21 @@
+public class TestMax {
+    public static int max(int num1,int num2){
+        int result;
+        if (num1>num2){
+            result=num1;
+
+        }else {
+            result =num2;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int i=90;
+        int j=45;
+
+        int k=max(i,j);
+
+        System.out.println("The max value between "+ i + " and " + j + " is " + k);
+
+    }
+}
