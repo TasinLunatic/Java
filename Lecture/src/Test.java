b81e1036b17351679df81afb7e1cad9dd3f337b3
TasinLunatic <ahmedtasin207@gmail.com>
2025-03-27 01:38:49 +0800

    Shortcut of max test

---
+++ Lecture/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+
+        int k =TestMax.max(i,j);
+
+        System.out.println(" The max of "+i+" and " +j+ " is "+k);
+    }
+}
