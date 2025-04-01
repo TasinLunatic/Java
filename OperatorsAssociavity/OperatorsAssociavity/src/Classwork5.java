e9df134a393dd58e04fe22fd1113a319facae92b
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-02 00:54:25 +0800

    loop and algorithm added

--- OperatorsAssociavity/OperatorsAssociavity/src/Classwork5.java
+++ OperatorsAssociavity/OperatorsAssociavity/src/Classwork5.java
@@ -8,6 +8,20 @@ public class Classwork5 {
         double investmentAmount = sc.nextDouble();
 
         System.out.println("Annual interest rate: ");
+        double annualInterestRate = sc.nextDouble();
 
+        double monthlyInterestRate = annualInterestRate/100/12;
+
+        System.out.println("Years future value");
+
+        for (int years=1; years<=30;years++){
+            double futureValue = futureInvestmentValue(investmentAmount,monthlyInterestRate,years);
+            System.out.printf("%-8d%.2f%n",years,futureValue);
+        }
+        sc.close();
+
+    }
+    public static double futureInvestmentValue(double investmentAmount,double monthlyInterestRate,int years){
+        return investmentAmount*
     }
 }
