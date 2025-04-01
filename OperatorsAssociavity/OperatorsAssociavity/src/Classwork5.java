ef5710f78c29b2b857d782ca31d9000d9294cc56
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-02 00:22:20 +0800

    First part done

---
+++ OperatorsAssociavity/OperatorsAssociavity/src/Classwork5.java
@@ -0,0 +1,18 @@
+public class Classwork5 {
+    public static void main(String[] args) {
+        final int number_of_primes =50 ;
+        final  int primes_per_line =10;
+        int count=0;
+        int number=2;
+
+        System.out.println("The first 50 primes number are : ");
+
+        while (count<number_of_primes){
+            boolean isprime=true;
+
+            for (int divisor =2;divisor <= Math.sqrt(number)){
+
+            }
+        }
+    }
+}
