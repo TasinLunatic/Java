6c40b6227e9c66f133e34e71bcba3eb04d221e96
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-03 00:40:54 +0800

    first step

---
+++ Array/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Classroom of 500 students.You have to store 500 students marks by using Array
+        int [] marks =new int[5];
+
+        marks[1]=100;
+        marks[2]=67;
+        marks[3]=70;
+        marks[4]=90;
+
+        System.out.println(marks[4]);
+    }
+}
