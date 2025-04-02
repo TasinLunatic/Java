68d5dacae4969c61c674e2b918a48eed8a9ec54c
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-03 00:48:43 +0800

    three way of declare added

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         //Classroom of 500 students.You have to store 500 students marks by using Array
         int [] marks =new int[5];
+        //int []marks ={98,45,68,76,87} ( Declaration,memory allocation and initialization)
+        //int []marks; marks=new int[5] (Declaration and memory allocation)
 
         marks[1]=100;
         marks[2]=67;
