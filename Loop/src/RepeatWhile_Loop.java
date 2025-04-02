77600d9900e8de439a9ddb8bd424620ee81b61bd
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-02 17:23:40 +0800

    factorial done

---
+++ Loop/src/RepeatWhile_Loop.java
@@ -0,0 +1,19 @@
+public class RepeatWhile_Loop {
+    public static void main(String[] args) {
+        //Repeat 5 using While Loop?
+        int n=5;
+        //What is factorial n=n*n-1*n-2....1
+        //5!=5*4*3*2*1
+
+        int i =1;
+        int factorial=1;
+
+        while (i<n){
+          factorial *=i;
+          i++;
+
+        }
+        System.out.println(factorial);
+
+    }
+}
