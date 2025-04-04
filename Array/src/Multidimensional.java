a2ac347b6090a1583995a6482dec889c6b46f7b9
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-04 21:53:39 +0800

    Visualization done

--- Array/src/Multidimensional.java
+++ Array/src/Multidimensional.java
@@ -2,6 +2,8 @@ public class Multidimensional {
     public static void main(String[] args) {
         int []marks; //1D Array
         int [][]flat ;//2D Array
+        //String [][][]arr=new String[1][2][4] 3D Array
+
         int [][]flats =new int[2][3];
 
         flats[0][0]=100;
@@ -12,12 +14,13 @@ public class Multidimensional {
         flats[1][2]=203;
 
         System.out.println("Printing 2D Array:");
-        for(int i=0;i< flats.length;i++){
+        for (int i=0;i< flats.length;i++){
             for (int j=0;j<flats[i].length;j++){
                 System.out.print(flats[i][j]);
                 System.out.print(" ");
             }
+            System.out.println("\n");
         }
-        System.out.println("\n");
+
     }
 }
