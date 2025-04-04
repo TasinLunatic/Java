c2ec8e64e3b99266b94ed3576c8386c8eb69de54
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-04 21:01:34 +0800

    Display done

---
+++ Loop/src/ForEachLoop.java
@@ -0,0 +1,12 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int [] marks ={49,98,87,76,89};
+        System.out.println(marks.length);
+        System.out.println(marks[2]);
+        //Display the Array by using for loop
+
+        for (int i=0;i< marks.length;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
