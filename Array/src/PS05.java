988a8f697a79ad72faecdf190f1e8c16d6e5ec8c
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 00:30:18 +0800

    1st done

--- Array/src/PS05.java
+++ Array/src/PS05.java
@@ -1,5 +1,12 @@
 public class PS05 {
     public static void main(String[] args) {
+       //Write a java program to reverse an Array?
+        int []arr={1,2,3,4,5,6};
+        int n=Math.floorDiv(args.length, 2);
+        System.out.println(n);
 
+        for (int i=0;i<n;i++){
+            //swap a[i] and a[l-1-i]
+        }
     }
 }
