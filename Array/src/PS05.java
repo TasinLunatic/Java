191e75dfc31856d638ad6a3c4c5d4f55e0546b60
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 00:38:10 +0800

    loop added and correction

--- Array/src/PS05.java
+++ Array/src/PS05.java
@@ -2,11 +2,16 @@ public class PS05 {
     public static void main(String[] args) {
        //Write a java program to reverse an Array?
         int []arr={1,2,3,4,5,6};
-        int n=Math.floorDiv(args.length, 2);
-        System.out.println(n);
+        int l= arr.length;
+        int n=Math.floorDiv(l, 2);
+
+        int temp;
 
         for (int i=0;i<n;i++){
             //swap a[i] and a[l-1-i]
+            temp=arr[i];
+            arr[i]=arr[l-1-i];
+            arr[l-1-i]=temp;
         }
     }
 }
