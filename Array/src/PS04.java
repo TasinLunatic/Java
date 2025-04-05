a7eb281d1d62ea7a57d0fcb1416f18c35c594001
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 00:08:54 +0800

    Matrix solved

--- Array/src/PS04.java
+++ Array/src/PS04.java
@@ -8,13 +8,22 @@ public class PS04 {
                     {4, 6, 8}};
         int [][] result={{0,0,0},
                          {0,0,0}};
+        for (int i=0;i< mat1.length;i++) {
+            for (int j = 0; j < mat1[i].length; j++) {
+                System.out.format("The value of i=%d and j=%d\n", i, j);
+                result[i][j] = mat1[i][j] + mat2[i][j];
+
+            }
+        }
+        System.out.println("\n");
 
         for (int i=0;i< mat1.length;i++){
             for (int j=0;j<mat1[i].length;j++){
-                System.out.format("Setting value for i=%d and j=%d\n",i,j);
+                System.out.print(result[i][j] +" ");
                 result[i][j]=mat1[i][j]+mat2[i][j] ;
 
             }
+            System.out.println("\n");
         }
     }
 }
---
+++ Array/src/PS05.java
@@ -0,0 +1,5 @@
+public class PS05 {
+    public static void main(String[] args) {
+
+    }
+}
