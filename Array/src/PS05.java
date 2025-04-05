b9a55d1a9cf3cb57ca0988a623bccdf9923246fa
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 01:10:46 +0800

    reverse done

--- Array/src/PS05.java
+++ Array/src/PS05.java
@@ -1,7 +1,7 @@
 public class PS05 {
     public static void main(String[] args) {
        //Write a java program to reverse an Array?
-        int []arr={1,2,3,4,5,6};
+        int []arr={1,2,3,4,5};
         int l= arr.length;
         int n=Math.floorDiv(l, 2);
 
@@ -9,9 +9,14 @@ public class PS05 {
 
         for (int i=0;i<n;i++){
             //swap a[i] and a[l-1-i]
+            //  a    b   temp
+            // |3| |4|   | |
             temp=arr[i];
             arr[i]=arr[l-1-i];
             arr[l-1-i]=temp;
+        }
+        for (int element:arr){
+            System.out.print(element+" ");
         }
     }
 }
