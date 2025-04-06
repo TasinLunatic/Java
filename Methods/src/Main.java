c552a3966da78a54cbb669efa37565311919a59f
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 22:38:31 +0800

    method added

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -1,23 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static int logic(int x,int y){
+        int z;
+        if (x>y){
+            z=x+y;
+        }else {
+            z=(x+y)*5;
+        }
+        return z;
+    }
     public static void main(String[] args) {
         int a=6;
         int b=7;
         int c;
-        if (a>b){
-            c=(a+b);
-        }else {
-            c=(a+b)*5;
-        }
+        c=logic(a,b);
         int a1=2;
         int b1=1;
         int c1;
-        if (a1>b1){
-            c1=(a1+b1);
-        }else{
-            c1=(a1+b1)*5;
-        }
+        c1=logic(a1,b1);
        System.out.println(c);
         System.out.println(c1);
     }
