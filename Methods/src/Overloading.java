4d2eeb7be499a6b31fc0f7b5ef0451aafb9281e3
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 23:01:53 +0800

    added new

--- Methods/src/Overloading.java
+++ Methods/src/Overloading.java
@@ -1,9 +1,16 @@
 public class Overloading {
+    static void change(int a){
+        a=98;
+    }
     static void telljoke(){
         System.out.println("I invented a new word!\n"+"plagiarism!");
     }
     public static void main(String[] args) {
-          telljoke();
-        }
+         //telljoke();
+        int[]marks={55,67,78,98,67};
+        int x=45;
+        System.out.println("The value of x after running is "+ x);
+    }
+
 
 }
