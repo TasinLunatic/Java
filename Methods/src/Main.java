f8a6013e87ea0134cb251607586a415a254c1d01
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 22:26:54 +0800

    condition check

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -4,7 +4,21 @@ public class Main {
     public static void main(String[] args) {
         int a=6;
         int b=7;
-        int c=(a+b)*5;
-        System.out.println(c);
+        int c;
+        if (a>b){
+            c=(a+b);
+        }else {
+            c=(a+b)*5;
+        }
+        int a1=2;
+        int b1=1;
+        int c1;
+        if (a1>b1){
+            c1=(a1+b1);
+        }else{
+            c1=(a1+b1)*5;
+        }
+       System.out.println(c);
+        System.out.println(c1);
     }
 }
