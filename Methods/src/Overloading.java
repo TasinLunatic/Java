73aae8d85902dbd020766c622bf3d8af1ebdd9c2
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-06 22:53:53 +0800

    normal test

---
+++ Methods/src/Overloading.java
@@ -0,0 +1,9 @@
+public class Overloading {
+    static void telljoke(){
+        System.out.println("I invented a new word!\n"+"plagiarism!");
+    }
+    public static void main(String[] args) {
+          telljoke();
+        }
+
+}
