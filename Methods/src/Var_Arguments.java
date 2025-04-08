a40e96d741043695ccc1ba032ad0ead8b2b39db7
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-08 21:03:10 +0800

    1st part done

---
+++ Methods/src/Var_Arguments.java
@@ -0,0 +1,9 @@
+public class Var_Arguments {
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to varargs Tutorial!");
+        System.out.println("The sum of 4 and 5 is:"+sum(4,5));
+    }
+}
