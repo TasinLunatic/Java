dded1a95f529080e8f1ea80385b899dfb461b92f
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-10 18:08:20 +0800

    Problem 1 solved

---
+++ Methods/src/PS01.java
@@ -0,0 +1,11 @@
+public class PS01 {
+    static void multiplication(int n){
+        for (int i=1;i<=10;i++){
+            System.out.format("%d X %d =%d",n,i,n*i);
+        }
+    }
+    public static void main(String[] args) {
+        //write a java method to print multiplication table of number n?
+        multiplication(7);
+    }
+}
