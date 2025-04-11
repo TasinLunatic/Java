5077fc994ef6c3585a56948fed251349e9ed43ef
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-12 00:16:53 +0800

    new trainer not working

--- Methods/src/Custom_class.java
+++ Methods/src/Custom_class.java
@@ -1,14 +1,20 @@
 public class Custom_class {
-    public class Employee{
+    class  trainer{
         int id;
         String name;
+        public void printDetails(){
+            System.out.println("My id is"+id);
+            System.out.println("and my name is"+name);
+        }
     }
     public static void main(String[] args) {
         System.out.println("This is our custom class!");
-        Employee tasin = new Employee();
+
+        trainer tasin =  new trainer();//instantiating a new Employee object.
         tasin.id=12;
-        tasin.name="TasinLunatic";
-        System.out.println(tasin.id);
-        System.out.println(tasin.name);
+        tasin.name="tasinLunatic";
+        //System.out.println(tasin.id);
+        //System.out.println(tasin.name);
+        tasin.printDetails();
     }
 }
