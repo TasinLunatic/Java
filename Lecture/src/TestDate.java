92d7b08d051466b3f64cc0a4647c4f9c764aa2b6
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-19 15:51:10 +0800

    date test done

---
+++ Lecture/src/TestDate.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class TestDate {
+    public static void main(String[] args) {
+        Date today= new Date();
+
+        System.out.println(today.toString());
+    }
+}
