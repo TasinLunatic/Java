f2e05d80405bdceb81c7264084883535f9eef0ed
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-19 15:49:01 +0800

    Testing done

--- Lecture/src/TestCircle.java
+++ Lecture/src/TestCircle.java
@@ -1,2 +1,11 @@
 public class TestCircle {
+    public static void main(String[] args) {
+        Circle myCircle = new Circle();
+        Circle yourCircle =new Circle(100);
+
+        System.out.println("Radius of my Circle is:"+myCircle.radius);
+        System.out.println("Area of my Circle is:"+myCircle.getArea());
+
+        //Print the radius and area of your circle
+    }
 }
