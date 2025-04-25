cb1fffc19d8e582e2dbe2311c418ac6fe650d6e3
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-25 22:57:18 +0800

    previous problem solved and new PS added

--- Methods/src/Custom_class.java
+++ Methods/src/Custom_class.java
@@ -1,16 +1,22 @@
-public class Custom_class {
-    class  trainer{
-        int id;
-        String name;
-        public void printDetails(){
-            System.out.println("My id is"+id);
-            System.out.println("and my name is"+name);
-        }
+class  trainer{
+    int id;
+    String name;
+    public void printDetails(){
+        System.out.println("My id is"+id);
+        System.out.println("and my name is"+name);
     }
+}
+
+
+
+
+
+public class Custom_class {
+
     public static void main(String[] args) {
         System.out.println("This is our custom class!");
 
-        trainer tasin =  new trainer();//instantiating a new Employee object.
+        trainer tasin = new trainer();//instantiating a new Employee object.
         tasin.id=12;
         tasin.name="tasinLunatic";
         //System.out.println(tasin.id);
---
+++ Methods/src/PS06.java
@@ -0,0 +1,32 @@
+class Employee1{
+    int salary;
+    String name;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void  setName(String n){
+        name=n;
+    }
+}
+
+
+
+
+public class PS06 {
+
+    public static void main(String[] args) {
+        //create a class employee with following properties and method
+        //salary,get salary,name,get name,set name
+
+        Employee1 tasin=new Employee1();
+        tasin.setName("bongodevTasin");
+        System.out.println(tasin.getName());
+
+    }
+}
