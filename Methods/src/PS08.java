eca3af9d26987cf9591fd0a2846284776b5fc94c
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-28 10:57:46 +0800

    created full version

--- Methods/src/PS08.java
+++ Methods/src/PS08.java
@@ -6,13 +6,17 @@ class Tommy{
         System.out.println("Running");
     }
     public void fire(){
-        System.out.println("Firing the enemy.");
+        System.out.println("Firing on the enemy.");
     }
 }
 
 public class PS08 {
     public static void main(String[] args) {
       //create a class for tommy for rockstar games capable of hitting,running,firing etc.
+      Tommy player1 =new Tommy();
+      player1.fire();
+      player1.hit();
+      player1.run();
 
     }
 }
