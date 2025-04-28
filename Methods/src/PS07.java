959484d42256858397744bdc0ee0507d5372b679
TasinLunatic <ahmedtasin207@gmail.com>
2025-04-28 10:32:19 +0800

    area and perimeter done

---
+++ Methods/src/PS07.java
@@ -0,0 +1,20 @@
+class square{
+    int side;
+    public  int area(){
+        return side*side;
+    }
+    public int perimeter(){
+        return 4*side;
+    }
+}
+
+
+public class PS07 {
+    public static void main(String[] args) {
+    // creat a class square with a method to initialized its side,area,perimeter etc.
+        square sq=new square();
+        sq.side =3;
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+    }
+}
