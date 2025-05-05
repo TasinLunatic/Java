7be4c876895afb9358278713a4e20ba1f5ae58b9
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-05 12:01:29 +0800

    testing done

---
+++ Access ModifiersandConstructors/src/Main.java
@@ -0,0 +1,13 @@
+class Employee{
+    int id;
+    String name;
+}
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       Employee Tasin =new Employee();
+       Tasin.id=67;
+       Tasin.name="Hacker";
+    }
+}
