fcae33edcbe576269274a23972fb0b5dfad9337b
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-05 12:10:32 +0800

    private modifier added

--- Access ModifiersandConstructors/src/Main.java
+++ Access ModifiersandConstructors/src/Main.java
@@ -1,13 +1,29 @@
 class Employee{
-    int id;
-    String name;
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name=n;
+    }
+    public void setId(int i){
+        id=i;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
        Employee Tasin =new Employee();
-       Tasin.id=67;
-       Tasin.name="Hacker";
+       //Tasin.id=67;
+       //Tasin.name="Hacker";[Throw an error during access private modifier]
+        Tasin.setName("Hacker");
+        System.out.println(Tasin.getName());
     }
 }
