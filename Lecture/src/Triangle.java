48dacd2be51e56f710a3089bd5380967d45c2bb7
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-06 22:16:24 +0800

    Triangle test done

--- Lecture/src/Triangle.java
+++ Lecture/src/Triangle.java
@@ -1,4 +1,4 @@
-public class TriangleExGeoMetricObject {
+public class Triangle extends GeoMetricObject {
     private double side1 = 1.0;
     private double side2 = 1.0;
     private double side3 = 1.0;
