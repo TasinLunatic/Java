e8c915b6e4150bec5f995f4ca2473eeba0c5cc6d
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-14 08:35:44 +0800

    Lecture animal done

---
+++ Lecture/src/Dog.java
@@ -0,0 +1,8 @@
+import abstract_class.Animal;
+
+public class Dog  extends Animal {
+    public void makeSound(){
+        System.out.println("Woof");
+    }
+
+}
---
+++ Lecture/src/main1.java
@@ -0,0 +1,8 @@
+public class main1 {
+    public static void main(String[] args) {
+        Dog dog=new Dog();
+
+        dog.makeSound();
+        dog.eat();
+    }
+}
