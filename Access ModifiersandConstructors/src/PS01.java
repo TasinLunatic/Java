730399672fc23eb175538f82c768e951f721d2c5
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-15 15:15:43 +0800

    problem1 solved

---
+++ Access ModifiersandConstructors/src/PS01.java
@@ -0,0 +1,33 @@
+class cylinder{
+    private int radius;
+    private int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
+
+
+public class PS01 {
+    public static void main(String[] args) {
+        //Create a cylinder class and use getters and setters to sets its radius and height?
+
+        cylinder myCylinder =new cylinder();
+        myCylinder.setHeight(45);
+        int h=myCylinder.getHeight();
+        System.out.println(h);
+
+    }
+}
