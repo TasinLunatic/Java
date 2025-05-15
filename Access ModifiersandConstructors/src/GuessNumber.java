00c9cb32783122aabc54e89bceb85d7f2f2e9cb4
TasinLunatic <ahmedtasin207@gmail.com>
2025-05-15 15:03:11 +0800

    Loop added and done

--- Access ModifiersandConstructors/src/GuessNumber.java
+++ Access ModifiersandConstructors/src/GuessNumber.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class GuessNumber {
     public static void main(String[] args) {
         //Create a class which allows guess the number?
-        Scanner scanner = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
         int numberToGuess = random.nextInt(100) + 1;
@@ -15,6 +16,30 @@ public class GuessNumber {
 
         System.out.println("Welcome to the Guess the Number Game!");
         System.out.println("I'm thinking of a number between 1 and 100. Can you guess it?");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+
+            if (!sc.hasNextInt()) {
+                System.out.println("Please enter a valid number!");
+                sc.next();
+                continue;
+            }
+
+            guess = sc.nextInt();
+            numberOfTries++;
+
+            if (guess < 1 || guess > 100) {
+                System.out.println("Please guess a number between 1 and 100.");
+            } else if (guess < numberToGuess) {
+                System.out.println("Too low!");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high!");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You guessed the number in " + numberOfTries + " tries.");
+            }
+        }
 
 
     }
