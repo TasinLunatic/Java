bc6f932362643fcea377b3d73d93b8c56db0e753
TasinLunatic <ahmedtasin207@gmail.com>
2025-06-03 13:15:06 +0800

    case study 7&8 added

--- Project-1/src/Case_study6/CountLettersInArray.java
+++ Project-1/src/Case_study6/CountLettersInArray.java
@@ -24,7 +24,6 @@ public class CountLettersInArray {
     public static char[] createArray(){
         //Declare an array of characters and create it
         char[] chars = new char[100];
-
         //Create lowercase letters randomly and assign them to the array
 
         for(int i = 0; i < chars.length; i++)
